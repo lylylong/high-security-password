fe0bb0bf87c33cfffc1d4930d9c2bdb811b99c98
Lyly Long <lylylong@gmail.com>
2020-09-20 01:52:23 -0400

    make sure password are 8 - 128 length all number

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,23 @@
 // Assignment code here
+var passwordLength = passwordLength > 8 && passwordLength < 128;
 
+var generatePassword = function () {
+  var passwordLength = parseInt(
+    window.prompt("Password Length: type from 8 to 128")
+  );
+  if (isNaN(passwordLength) === true) {
+    window.alert("You must provide a NUMBER from 8 to 128");
+    generatePassword();
+  }
+  if (passwordLength < 8) {
+    window.alert("You must provide a NUMBER from 8 to 128");
+    generatePassword();
+  }
+  if (passwordLength > 128) {
+    window.alert("You must provide a NUMBER from 8 to 128");
+    generatePassword();
+  }
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,7 +28,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
