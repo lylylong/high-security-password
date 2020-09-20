e873e0bd898bd5b64082e495582ebfe002f1f357
Lyly Long <lylylong@gmail.com>
2020-09-20 16:35:22 -0400

    finish the js stricture

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,19 @@
 // Assignment code here
-var passwordLength = passwordLength > 8 && passwordLength < 128;
-var pecialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "?"];
+// setup all arr
+var specialCharacter = [
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "?",
+  "-",
+  "+",
+  "_",
+];
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var lowercase = [
   "a",
@@ -59,37 +72,96 @@ var uppercase = [
   "Z",
 ];
 
-var generatePassword = function () {
-  var passwordLength = parseInt(
-    window.prompt("Password Length: type from 8 to 128")
-  );
-  if (isNaN(passwordLength) === true) {
-    window.alert("You must provide a NUMBER from 8 to 128");
-    generatePassword();
+// design the password types
+var passwordTypes = function () {
+  var length = parseInt(prompt("Type your password length: choose 8 - 128"));
+  if (length < 8) {
+    alert("You must provide a NUMBER at least 8, please start over!");
+    return;
   }
-  if (passwordLength < 8) {
-    window.alert("You must provide a NUMBER from 8 to 128");
-    generatePassword();
+  if (length > 128) {
+    alert("You must provide a NUMBER no more than 128, please start over!");
+    return;
   }
-  if (passwordLength > 128) {
-    window.alert("You must provide a NUMBER from 8 to 128");
-    generatePassword();
+  console.log("The password length is " + length);
+  if (isNaN(length) === true) {
+    alert("You must provide a NUMBER from 8 to 128, please start over!");
+    return;
   }
-  console.log("The password length is " + passwordLength);
 
   var specialCharacterInclude = confirm("Include special character(s)?");
   console.log(
-    "Password will include special character(s) - " + specialCharacterInclude
+    "Password will include special character(s): " + specialCharacterInclude
   );
 
   var numberInclude = confirm("Include number(s)?");
-  console.log("Password will include number(s) - " + numberInclude);
+  console.log("Password will include number(s): " + numberInclude);
 
   var lowercaseInclude = confirm("Include lowercase(s)?");
-  console.log("Password will include lowercase(s) - " + lowercaseInclude);
+  console.log("Password will include lowercase(s): " + lowercaseInclude);
 
   var uppercaseInclude = confirm("Include uppercase(s)?");
-  console.log("Password will include uppercase(s) - " + uppercaseInclude);
+  console.log("Password will include uppercase(s): " + uppercaseInclude);
+
+  if (
+    specialCharacterInclude === false &&
+    numberInclude === false &&
+    lowercaseInclude === false &&
+    uppercaseInclude === false
+  ) {
+    alert(
+      "You must include at least one type of characters, please start over!"
+    );
+    return;
+  }
+
+  var passwordTypes = {
+    length: length,
+    specialCharacterInclude: specialCharacterInclude,
+    numberInclude: numberInclude,
+    lowercaseInclude: lowercaseInclude,
+    uppercaseInclude: uppercaseInclude,
+  };
+  return passwordTypes;
+};
+
+var getRandom = function (arr) {
+  var randomIndex = Math.floor(Math.random() * arr.length);
+  var randomElement = arr[randomIndex];
+  return randomElement;
+};
+
+var generatePassword = function () {
+  var types = passwordTypes();
+  var result = [];
+  var possibleCharacterAll = [];
+  var guaranteedCharacterAll = [];
+
+  if (types.specialCharacterInclude) {
+    possibleCharacterAll = possibleCharacterAll.concat(specialCharacter);
+    guaranteedCharacterAll.push(getRandom(specialCharacter));
+  }
+  if (types.numberInclude) {
+    possibleCharacterAll = possibleCharacterAll.concat(number);
+    guaranteedCharacterAll.push(getRandom(number));
+  }
+  if (types.lowercaseInclude) {
+    possibleCharacterAll = possibleCharacterAll.concat(lowercase);
+    guaranteedCharacterAll.push(getRandom(lowercase));
+  }
+  if (types.uppercaseInclude) {
+    possibleCharacterAll = possibleCharacterAll.concat(uppercase);
+    guaranteedCharacterAll.push(getRandom(uppercase));
+  }
+  for (var i = 0; i < types.length; i++) {
+    var possibleCharacter = getRandom(possibleCharacterAll);
+
+    result.push(possibleCharacter);
+  }
+  for (var i = 0; i < guaranteedCharacterAll.length; i++) {
+    result[i] = guaranteedCharacterAll[i];
+  }
+  return result.join("");
 };
 
 // Get references to the #generate element
