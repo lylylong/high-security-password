15da568480f72efbb1f6b8ffbbe01a26ead2a1b3
Lyly Long <lylylong@gmail.com>
2020-09-20 02:26:28 -0400

    make sure password types, including lowercase, uppercase, numeric, and/or special characters?

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,63 @@
 // Assignment code here
 var passwordLength = passwordLength > 8 && passwordLength < 128;
+var pecialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "?"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowercase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var uppercase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 
 var generatePassword = function () {
   var passwordLength = parseInt(
@@ -17,6 +75,21 @@ var generatePassword = function () {
     window.alert("You must provide a NUMBER from 8 to 128");
     generatePassword();
   }
+  console.log("The password length is " + passwordLength);
+
+  var specialCharacterInclude = confirm("Include special character(s)?");
+  console.log(
+    "Password will include special character(s) - " + specialCharacterInclude
+  );
+
+  var numberInclude = confirm("Include number(s)?");
+  console.log("Password will include number(s) - " + numberInclude);
+
+  var lowercaseInclude = confirm("Include lowercase(s)?");
+  console.log("Password will include lowercase(s) - " + lowercaseInclude);
+
+  var uppercaseInclude = confirm("Include uppercase(s)?");
+  console.log("Password will include uppercase(s) - " + uppercaseInclude);
 };
 
 // Get references to the #generate element
